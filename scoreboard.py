b64634da7a49f38c640c931bf9d0ec72a44b179d
Harishkumarharry <54526806+Harishkumarharry@users.noreply.github.com>
2023-06-12 20:51:31 -0700

    Added High Score

--- scoreboard.py
+++ scoreboard.py
@@ -9,16 +9,26 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt", 'r') as file:
+            self.high_score = int(file.read())
         self.goto(0, 280)
         self.color("red")
         self.hideturtle()
         self.score_increase()
 
     def score_increase(self):
-        self.clear()
-        self.write(f"Score : {self.score}", False, align=ALIGNMENT, font=FONT)
+        self.update_scoreboard()
         self.score += 1
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", False, align=ALIGNMENT, font=FONT)
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score : {self.score} High Score: {self.high_score}", False, align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", 'w') as file:
+                file.write(str(self.high_score))
+        self.score = 0
+        self.update_scoreboard()
+
